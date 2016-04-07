f43b13bc86ac54454d304d294a9219b2f98bf8d7
Nikita Khomyakov <dr.evil@krtn.eu>
2016-04-08 02:47:09 +0300

    UMD definition, more formats, browser support
    Simple playlist format support added. Format of returned value
    is the same but with `title` and `duration` properties being
    null.
    
    Code was revised in order to make library work in browsers as well.
    
    Callbacks can now be used to get parsed playlist. However, if no callback was provided, Promise will be returned.
    
    Odd Bluebird dependency is removed.

--- src/m3u.js
+++ src/m3u.js
@@ -1,46 +1,117 @@
-var Promise = require('bluebird');
+(function (root, factory) {
+    /* istanbul ignore next */
+    if (typeof define === 'function' && define.amd)
+        define([], factory);
+    else if (typeof module === 'object' && module.exports)
+        module.exports = factory();
+    else
+        root.m3uParser = factory();
+}(this, function () {
+    'use strict';
 
-function parse (data) {
-    if (Buffer.isBuffer(data))
-        data = data.toString();
-    else if (typeof data !== 'string')
-        return Promise.reject(new TypeError('Data passed to the parser should be a string'));
+    function parse (data, cb) {
+        const promise = typeof cb !== 'function',
+              resolve = promise ? Promise.resolve.bind(Promise) : (r => cb(null, r)),
+              reject  = promise ? Promise.reject.bind(Promise) : cb;
 
-    return new Promise(function (resolve, reject) {
-        data = data.split('\n')
-            .filter(function (str) {
-                return str.length > 0;
-            });
+        if (typeof Buffer === 'function' && Buffer.isBuffer(data))
+            data = data.toString();
+        else if (typeof data !== 'string')
+            return reject(new TypeError('Data passed to the parser should be a string'));
 
-        if (data.shift().trim() !== '#EXTM3U')
-            return reject(new Error('Passed data is not valid M3U playlist'));
 
-        var buffer = [], isWaitingForLink = false, line;
+        data = data.split('\n').filter(str => str.length > 0);
+
+        if (data.length === 0)
+            return resolve([]);
+
+
+        if (data[0][0] === '#') {
+            const line = data.shift().trim();
+            if (line !== '#EXTM3U')
+                return reject(new Error('Unsupported playlist format'));
+        } else
+            return resolve(data.map(file => ({ file, title: null, duration: null })));
+
+
+        const buffer = [];
+        let line;
 
         while ((line = data.shift())) {
             line = line.trim();
 
-            if (isWaitingForLink) {
-                buffer[buffer.length - 1].file = line;
-                isWaitingForLink = false;
-            } else if (line.slice(0, 7) === '#EXTINF') {
-                var result = /^#EXTINF:(-?)(\d+),(.*)$/.exec(line);
-                if (!result)
-                    throw new Error('Invalid M3U format');
+            if (buffer.length === 0)
+                buffer.push({ file: null, title: null, duration: null });
 
-                buffer.push({
-                    title:    result[3].trim(),
-                    duration: +(result[1] + result[2].trim())
-                });
+            const item = buffer[buffer.length - 1];
 
-                isWaitingForLink = true;
-            } else {
-                throw new Error('Invalid data');
-            }
+            if (line[0] === '#') {
+                if (line.slice(1, 4) === 'EXT') {
+                    const colonPos = line.indexOf(':', 4);
+
+                    if (colonPos === -1)
+                        return reject(new Error('#EXT tag used but no data provided'));
+
+                    const tagName = line.slice(4, colonPos),
+                          value   = line.slice(colonPos + 1).trim();
+
+                    switch (tagName) {
+                        case 'INF':
+                        {
+                            const commaPos = value.lastIndexOf(',');
+
+                            if (commaPos === -1)
+                                return reject(new Error('Invalid format of #EXTINF - unable to parse title'));
+
+                            item.title = value.slice(commaPos + 1).trim();
+
+                            let match = /^(-?[0-9]+)/.exec(value);
+
+                            if (!match)
+                                return reject(new Error('Invalid format of #EXTINF - unable to parse duration'));
+
+                            item.duration = +match[1];
+
+                            const EXTINF_ATTR_REGEX = / ([A-z0-9_-]+)="(.+?)"/g,
+                                  details           = value.slice(match[1].length, commaPos);
+
+                            while ((match = EXTINF_ATTR_REGEX.exec(details)) !== null) {
+                                if (!item.hasOwnProperty('attributes'))
+                                    item.attributes = {};
+
+                                item.attributes[match[1]] = match[2];
+                            }
+
+                            break;
+                        }
+                        default:
+                            item[line.slice(0, colonPos)] = value; // todo get value
+                            break;
+                    }
+                } else {
+                    // todo process unknown tags
+                }
+            } else if (item.file === null && item.title !== null) {
+                item.file = line;
+                buffer.push({ file: null, title: null, duration: null });
+            } else
+                return reject(new Error('Invalid data'));
         }
 
-        resolve(buffer);
-    });
-}
 
-module.exports.parse = parse;
+        let item = buffer[buffer.length - 1];
+
+        if (item.title === null && item.duration === null && item.file === null)
+            buffer.pop();
+
+        item = buffer[buffer.length - 1];
+
+        if (item.title === null || item.duration === null || item.file === null)
+            return reject(new Error('Invalid playlist'));
+
+
+        return resolve(buffer);
+    }
+
+    return { parse };
+}));
